979bd259c82a36726c369d83111e54eea958db68
Marcelo <marcelopintoarantes@gmail.com>
2025-02-01 03:16:12 +0000

    definições das váriaveis no struct

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,34 +1,6 @@
 #include <stdio.h>
 
-int main() {
-
-    //necessita de otimização
-    char pais[20];
-    char estado1='A';
-    char estado2='B';
-    char estado3='C';
-    char estado4='D';
-    char estado5='E';
-    char estado6='F';
-    char estado7='G';
-    char estado8='H';
-    int  cidade1=1;
-    int  cidade2=2;
-    int  cidade3=3;
-    int  cidade4=4;
-    int populacaoo;
-    float area;
-    double pib;
-    int npt;
-
-    printf("Bem vindo ao SuperTrunfo - Países!\n");
-    printf("Nesse jogo você cria cartas de países e suas propíedades, depois compara com as cartas de outros jogadores, vencerá quem tiver a carta mais forte!\n");
-    printf("Digite o nome do País\n");
-    scanf("%s", &pais);
-    printf("Seu País é: %s\n", pais);
-
-
-
-   return 0;
-
-}
+    int main() {
+        printf("Hello World");
+        return 0;
+    };
